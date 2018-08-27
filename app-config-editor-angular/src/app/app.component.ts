08c63d6f4ade4bd741a927682d01c828a9ad0f6d
Preston Lamb <plamb@hsahealthplan.com>
2018-08-27 16:59:28 -0600

    Generate the sails app and the angular app

---
+++ app-config-editor-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app-config-editor-angular';
+}
