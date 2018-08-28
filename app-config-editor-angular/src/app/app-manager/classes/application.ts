15fe1ad651dfe3c1d534951901d08d3c4526aaf6
Preston Lamb <preston.j.lamb@gmail.com>
2018-08-27 23:47:56 -0600

    add the id to the application class, and change the create application button styles

--- app-config-editor-angular/src/app/app-manager/classes/application.ts
+++ app-config-editor-angular/src/app/app-manager/classes/application.ts
@@ -1,9 +1,11 @@
 export class Application {
+	id: string;
 	name: string;
 	details: string;
 	serverDetails: string;
 
 	constructor(obj: any = {}) {
+		this.id = obj.id ? obj.id : null;
 		this.name = obj.name ? obj.name : '';
 		this.details = obj.details ? obj.details : '';
 		this.serverDetails = obj.serverDetails ? obj.serverDetails : '';
