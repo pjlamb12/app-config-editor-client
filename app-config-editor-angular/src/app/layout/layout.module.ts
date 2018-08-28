873c94b5494070171056e84988d430f1fc2da5bb
Preston Lamb <preston.j.lamb@gmail.com>
2018-08-28 00:37:00 -0600

    link properly in the navigation

--- app-config-editor-angular/src/app/layout/layout.module.ts
+++ app-config-editor-angular/src/app/layout/layout.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, RouterModule],
 	exports: [NavigationComponent, FooterComponent],
 	declarations: [NavigationComponent, FooterComponent],
 })
