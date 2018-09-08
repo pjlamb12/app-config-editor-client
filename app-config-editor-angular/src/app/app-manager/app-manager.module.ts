a4b34cfb905678798ca0aa4ed974b7ec5de71339
Preston Lamb <preston.j.lamb@gmail.com>
2018-09-08 00:43:34 -0600

    autosize text area

--- app-config-editor-angular/src/app/app-manager/app-manager.module.ts
+++ app-config-editor-angular/src/app/app-manager/app-manager.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgLoadersModule } from 'ng-loaders';
+import { TextareaAutosizeModule } from 'ngx-textarea-autosize';
 
 import { AppManagerRoutingModule } from './app-manager-routing.module';
 import { AppListComponent } from './app-list/app-list.component';
@@ -14,7 +15,15 @@ import { AppManagerService } from './app-manager.service';
 import { AppFormComponent } from './app-form/app-form.component';
 
 @NgModule({
-	imports: [CommonModule, RouterModule, HttpClientModule, FormsModule, NgLoadersModule, AppManagerRoutingModule],
+	imports: [
+		CommonModule,
+		RouterModule,
+		HttpClientModule,
+		FormsModule,
+		NgLoadersModule,
+		TextareaAutosizeModule,
+		AppManagerRoutingModule,
+	],
 	declarations: [AppListComponent, LandingComponent, AppFullDetailComponent, AppDetailsComponent, AppFormComponent],
 	providers: [AppManagerService],
 })
